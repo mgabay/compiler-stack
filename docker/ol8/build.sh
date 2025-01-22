79912b917142f259e1deec81fddbdb886d8a22a8
mgabay <michael.gabay@artelys.com>
2025-01-22 15:16:24 +0100

    update build examples

--- docker/ol8/build.sh
+++ docker/ol8/build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-docker buildx bake --pull --load x64-amd x64-intel
+# Useful build commands
+docker buildx bake --load --set *.platform=linux/amd64 x64-amd
+#docker buildx bake --pull --load --set *.platform=linux/amd64 x64-intel
+#docker buildx bake --pull --load --set *.platform=linux/amd64 x64-amd x64-intel
+#docker buildx bake --pull --load amd64-acfl
 
 # For multi-platform build, need buildx multi-platform builder:
 # docker buildx create --use
