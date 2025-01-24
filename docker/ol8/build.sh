426c9b8a8f660e0cb3965a5cc1e9c8651fcabacd
mgabay <michael.gabay@artelys.com>
2025-01-24 14:33:49 +0100

    update ninja to 1.12.1

--- docker/ol8/build.sh
+++ docker/ol8/build.sh
@@ -6,6 +6,11 @@ docker buildx bake --load --set *.platform=linux/amd64 x64-amd
 #docker buildx bake --pull --load --set *.platform=linux/amd64 x64-amd x64-intel
 #docker buildx bake --pull --load amd64-acfl
 
+# To run multi-arch containers and builders:
+#sudo apt-get install qemu binfmt-support qemu-user-static # Install qemu packages
+#docker run --rm --privileged multiarch/qemu-user-static --reset -p yes # Execute registering scripts
+#docker run --platform=linux/arm64 --rm -t mgabay/compiler-stack:arm-2024.2 uname -m
+
 # For multi-platform build, need buildx multi-platform builder:
-# docker buildx create --use
-# docker buildx build --platform=linux/amd64,linux/arm64 .
+#docker buildx create --use
+#docker buildx build --platform=linux/amd64,linux/arm64 .
